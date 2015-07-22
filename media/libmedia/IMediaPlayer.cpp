440793ce1d5167694e80410e206802b79f6efd7c
Lianghu Su <lianghu.su@amlogic.com>
2015-07-22 14:11:58 +0800

    pd #108768: fix current position is wrong when loading online videos.
    Change-Id: If0c77d66488fcbacafdbfe530d7f2fc85f20d80b

--- media/libmedia/IMediaPlayer.cpp
+++ media/libmedia/IMediaPlayer.cpp
@@ -438,7 +438,7 @@ status_t BnMediaPlayer::onTransact(
         } break;
         case GET_CURRENT_POSITION: {
             CHECK_INTERFACE(IMediaPlayer, data, reply);
-            int msec;
+            int msec = 0;
             status_t ret = getCurrentPosition(&msec);
             reply->writeInt32(msec);
             reply->writeInt32(ret);
