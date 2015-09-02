0d4b3958c68d144329382ceb42a52e5d8fb3884a
Rico Yang <wei.yang@amlogic.com>
2015-09-02 10:28:17 +0800

    PD#111370: return OK in QueryCodec
    Change-Id: I3d3bbb89387290d3e7e3b8443c1a1c799a88c38e

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -5240,6 +5240,10 @@ status_t QueryCodec(
         const char *componentName, const char *mime,
         bool isEncoder,
         CodecCapabilities *caps) {
+
+    /*by AML*/
+    return OK;
+
     bool isVideo = !strncasecmp(mime, "video/", 6);
 
     sp<OMXCodecObserver> observer = new OMXCodecObserver;
